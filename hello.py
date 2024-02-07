b08709a46fcb5c0202acf5429716998c49dd102f
nefrance <nerea0980@gmail.com>
2024-02-07 22:19:20 +0100

    Hola Python

---
+++ hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario:
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python 2')
+
+"""
+Este es un
+comentario
+de varias
+líneas
+"""
+
+'''
+Este también es
+un comentario 
+en varias
+líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(5.7)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
