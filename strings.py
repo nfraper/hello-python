7a0f29cdc7dd9b9667b8bf819b226b815e113468
Nerea Francés Pérez <83375051+scarletnfp@users.noreply.github.com>
2024-02-15 19:40:19 +0100

    Añadimos fichero strings.py

---
+++ strings.py
@@ -0,0 +1,9 @@
+my_string = "Este es el primer string"
+my_other_string = "Este es mi segundo string"
+
+print("1. " + my_string + " " + my_other_string)
+print("2." ,len(my_string))
+
+print("3. " + my_string + "\nEsto es un salto de línea")
+print("4. " + my_string + "\n\tEsto es un tab")
+print("5. \\t Imprimimos todo \\n")
