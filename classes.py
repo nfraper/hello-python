44531d3dfc4a266b3f64b3fa25518942c858696d
Nerea Francés Pérez <83375051+scarletnfp@users.noreply.github.com>
2024-02-18 22:08:39 +0100

    Classes lecture 2

--- classes.py
+++ classes.py
@@ -8,17 +8,19 @@ print(MyEmptyPerson())
 
 class Person:
     def __init__(self, name, surname): #constructor de clase
-        self.name = name
-        self.surname = surname
+        self.name = name 
+        self.__surname = surname #Propiedad privada 
         self.full_name = f"{name} {surname}"
 
     def walk (self):
         print(f"{self.full_name} está caminando.")
     
+    def getSurname (self): #getter
+        return self.__surname
 
 
 my_person = Person("Nerea", "Francés")
-print(f"{my_person.name} {my_person.surname}")
+print(f"{my_person.name} {my_person.getSurname()}")
 
 print(my_person.full_name)
 
